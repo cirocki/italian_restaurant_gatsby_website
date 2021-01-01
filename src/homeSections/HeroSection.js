d1656e2ed4c6ce32a9cb84e686317d9eda65030b
cirocki <marcincirocki89@gmail.com>
2021-01-01 15:56:27 +0100

    hero section media queries

--- src/homeSections/HeroSection.js
+++ src/homeSections/HeroSection.js
@@ -1,22 +1,23 @@
-import React from "react"
+import React, { useRef, useState, useEffect } from "react"
 import styled from "styled-components"
-
 import Container from "../layout/container/Container"
-
+import { gsap } from "gsap"
 import heroBg from "../img/index/mondello-hero-image.jpg"
 
 const StyledSection = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
   margin-top: 60px;
   min-height: calc(100vh - 60px);
   max-height: 1040px;
   background: ${props => props.theme.colors.dark};
   background: url(${heroBg});
+  background-position: center center;
   background-repeat: no-repeat;
   background-size: cover;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
+  background-attachment: fixed;
   &:before {
     content: "";
     position: absolute;
@@ -37,21 +38,62 @@ const StyledHeading = styled.h1`
   font-family: ${props => props.theme.fonts.secondary};
   font-size: 100px;
   text-transform: uppercase;
+  @media (max-width: 768px) {
+    font-size: 80px;
+  }
+  @media (max-width: 489px) {
+    font-size: 60px;
+    padding-bottom: 20px;
+  }
+  @media (max-width: 374px) {
+    font-size: 48px;
+  }
 `
 const StyledSubheading = styled.h1`
   color: ${props => props.theme.colors.gold};
   font-family: ${props => props.theme.fonts.secondary};
   font-size: 2rem;
   font-style: italic;
+  @media (max-width: 768px) {
+    font-size: 1.75rem;
+  }
+  @media (max-width: 489px) {
+    font-size: 1.5rem;
+  }
+  @media (max-width: 374px) {
+    font-size: 1.25rem;
+  }
 `
 
 export default function HeroSection() {
+  let headingRef = useRef(null)
+  let subheadingRef = useRef(null)
+  const [heroTL] = useState(gsap.timeline())
+
+  useEffect(() => {
+    heroTL
+      .from(headingRef.current, {
+        autoAlpha: 0,
+        y: -100,
+        ease: "power1.inOut",
+        duration: 0.5,
+      })
+      .from(subheadingRef.current, {
+        autoAlpha: 0,
+        y: 50,
+        ease: "power4.inOut",
+        duration: 1,
+      })
+  }, [])
+
   return (
     <StyledSection>
       <Container>
         <StyledHeaderWrapper>
-          <StyledHeading>Mondello</StyledHeading>
-          <StyledSubheading>Explore the real taste of Italy!</StyledSubheading>
+          <StyledHeading ref={headingRef}>Mondello</StyledHeading>
+          <StyledSubheading ref={subheadingRef}>
+            Explore the real taste of Italy!
+          </StyledSubheading>
         </StyledHeaderWrapper>
       </Container>
     </StyledSection>
