3d1a0a6391d79063a4f3d9d8e1d6cf5b948a917f
cirocki <marcincirocki89@gmail.com>
2021-01-03 17:56:18 +0100

    reservation section media quieries

--- src/components/PrimaryHeading.js
+++ src/components/PrimaryHeading.js
@@ -2,7 +2,8 @@ import React from "react"
 import styled from "styled-components"
 
 const StyledHeading = styled.h2`
-  color: ${props => props.theme.colors.dark};
+  color: ${props =>
+    props.whiteColor ? props.theme.colors.white : props.theme.colors.dark};
   font-family: ${props => props.theme.fonts.secondary};
   font-size: 1.5rem;
   line-height: 1.7;
@@ -15,6 +16,10 @@ const StyledHeading = styled.h2`
   }
 `
 
-export default function PrimaryHeading({ children, margin }) {
-  return <StyledHeading margin={margin}>{children}</StyledHeading>
+export default function PrimaryHeading({ children, margin, whiteColor }) {
+  return (
+    <StyledHeading whiteColor={whiteColor} margin={margin}>
+      {children}
+    </StyledHeading>
+  )
 }
--- src/homeSections/ReservationSection.js
+++ src/homeSections/ReservationSection.js
@@ -6,11 +6,21 @@ import SecondaryHeading from "../components/SecondaryHeading"
 import ButtonGhost from "../components/ButtonGhost"
 import MainParagraph from "../components/MainParagraph"
 
+const StyledGridWrapper = styled.div`
+  @media (max-width: 1069px) {
+    display: flex;
+    justify-content: center;
+  }
+`
 const StyledGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(12, 1fr);
   grid-template-rows: repeat(4, 1fr);
-  min-height: 480px;
+  @media (max-width: 1069px) {
+    display: flex;
+    flex-direction: column;
+    max-width: 480px;
+  }
 `
 const StyledHeaderWrapper = styled.div`
   grid-column: 5/9;
@@ -19,13 +29,30 @@ const StyledHeaderWrapper = styled.div`
   text-align: center;
   background: ${props => props.theme.colors.dark};
   margin-top: -120px;
-  max-height: 480px;
+  @media (max-width: 1439px) {
+    grid-column: 2/7;
+  }
+  @media (max-width: 1279px) {
+    padding: 80px;
+  }
+  @media (max-width: 1069px) {
+    padding: 120px;
+  }
+  @media (max-width: 480px) {
+    padding: 80px;
+  }
+  @media (max-width: 360px) {
+    padding: 60px;
+  }
 `
 const StyledGreyDiv = styled.div`
   grid-column: 3/7;
   grid-row: 1/5;
   z-index: -1;
   background: ${props => props.theme.colors.light};
+  @media (max-width: 1439px) {
+    grid-column: 3/8;
+  }
 `
 const StyledMoreWrapper = styled.div`
   grid-column: 10/13;
@@ -33,28 +60,44 @@ const StyledMoreWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  @media (max-width: 1439px) {
+    grid-column: 9/13;
+  }
+  @media (max-width: 1069px) {
+    padding: 80px;
+  }
+  @media (max-width: 480px) {
+    padding: 40px;
+  }
+`
+const StyledPhone = styled.a`
+  padding-top: 1rem;
+  font-size: 1.5rem;
+  color: ${props => props.theme.colors.gold};
 `
 
 export default function ReservationSection() {
   return (
     <section>
       <Container>
-        <StyledGrid>
-          <StyledHeaderWrapper>
-            <SecondaryHeading>Reservation</SecondaryHeading>
-            <PrimaryHeading>
-              Try our perfect food. Book a table online.
-            </PrimaryHeading>
-            <ButtonGhost>Reserve a table</ButtonGhost>
-          </StyledHeaderWrapper>
-          <StyledGreyDiv></StyledGreyDiv>
-          <StyledMoreWrapper>
-            <MainParagraph>
-              You can also call to make a reservation.
-            </MainParagraph>
-            <h1>+1 737 6787 164</h1>
-          </StyledMoreWrapper>
-        </StyledGrid>
+        <StyledGridWrapper>
+          <StyledGrid>
+            <StyledHeaderWrapper>
+              <SecondaryHeading>Reservation</SecondaryHeading>
+              <PrimaryHeading whiteColor margin={80}>
+                Try our perfect food. Book a table online.
+              </PrimaryHeading>
+              <ButtonGhost>Reserve a table</ButtonGhost>
+            </StyledHeaderWrapper>
+            <StyledGreyDiv></StyledGreyDiv>
+            <StyledMoreWrapper>
+              <MainParagraph>
+                You can also call to make a reservation.
+              </MainParagraph>
+              <StyledPhone href="tel:7376787164">+1 737 6787 164</StyledPhone>
+            </StyledMoreWrapper>
+          </StyledGrid>
+        </StyledGridWrapper>
       </Container>
     </section>
   )
