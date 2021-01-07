ec09ef8c1da0334b481a7e9ac5006a6fcebb1c52
cirocki <marcincirocki89@gmail.com>
2021-01-07 17:57:18 +0100

    short menu styling

--- src/components/menuCard/ShortMenu.js
+++ src/components/menuCard/ShortMenu.js
@@ -1,8 +1,8 @@
 import React from "react"
-import { menuItemsData } from "../../data/MenuWithPrices"
 import styled from "styled-components"
+import { menuItemsData } from "../../data/MenuWithPrices"
 
-const StyledItem = styled.div`
+const StyledItem = styled.li`
   padding: 0.2rem 0;
   display: flex;
   justify-content: space-between;
@@ -15,14 +15,14 @@ const StyledItem = styled.div`
     left: 0;
     top: 50%;
     width: 100%;
-    z-index: -1;
     height: 100%;
+    z-index: -1;
   }
 `
 const StyledItemName = styled.p`
-  color: ${props => props.theme.colors.white};
   padding: 0.5rem 1rem 0.5rem 0;
   background: ${props => props.theme.colors.dark};
+  color: ${props => props.theme.colors.white};
   font-family: ${props => props.theme.fonts.secondary};
   font-size: 1.125rem;
   text-transform: uppercase;
@@ -31,9 +31,9 @@ const StyledItemName = styled.p`
   }
 `
 const StyledItemPrice = styled.p`
-  color: ${props => props.theme.colors.white};
   padding: 0.5rem 0 0.5rem 1rem;
   background: ${props => props.theme.colors.dark};
+  color: ${props => props.theme.colors.white};
   font-size: 1.125rem;
   letter-spacing: 1px;
   @media (max-width: 419px) {
@@ -42,13 +42,13 @@ const StyledItemPrice = styled.p`
 `
 export default function ShortMenu() {
   return (
-    <div>
+    <ul>
       {menuItemsData.map(item => (
         <StyledItem key={item.name}>
           <StyledItemName>{item.name}</StyledItemName>
           <StyledItemPrice>{item.price}</StyledItemPrice>
         </StyledItem>
       ))}
-    </div>
+    </ul>
   )
 }
