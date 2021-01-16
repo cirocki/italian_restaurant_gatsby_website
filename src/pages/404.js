52533b2677b5c2dcb662dff6db4fa2490864bfdb
cirocki <marcincirocki89@gmail.com>
2021-01-16 20:04:15 +0100

    Add 404 custom page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,25 @@
 import React from "react"
+import Layout from "../layout/Layout"
+import { Helmet } from "react-helmet"
+import styled from "styled-components"
+import Container from "../layout/container/Container"
+
+const StyledMainDiv = styled.main`
+  padding: 4rem 0;
+`
 
 export default function PageNotFound() {
   return (
-    <div>
-      <h1>Strony nie ma</h1>
-    </div>
+    <Layout>
+      <Helmet>
+        <title>Page Not Found | Mondello Restaurant</title>
+      </Helmet>
+      <StyledMainDiv>
+        <Container>
+          <h1>404</h1>
+          <h2>Page Not Found</h2>
+        </Container>
+      </StyledMainDiv>
+    </Layout>
   )
 }
