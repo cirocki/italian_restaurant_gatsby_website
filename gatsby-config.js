1c76b60810082dcc790d4d6cc3d0bf144c4f21e9
cirocki <marcincirocki89@gmail.com>
2021-01-18 13:47:28 +0100

    add custom favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,18 @@ module.exports = {
       },
     },
 
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "React go",
+        short_name: "Reactgo",
+        start_url: "/",
+        background_color: "#f7f0eb",
+        theme_color: "#a2466c",
+        display: "standalone",
+        icon: "src/img/favicon/mondello-favicon-white.svg",
+      },
+    },
     `gatsby-plugin-transition-link`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
