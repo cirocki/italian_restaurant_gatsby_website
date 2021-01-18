9c344693f5438092ac98c90c8564dd9c6973df9b
cirocki <marcincirocki89@gmail.com>
2021-01-18 14:12:54 +0100

    404 edit

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,6 @@ import React from "react"
 import Layout from "../layout/Layout"
 import { Helmet } from "react-helmet"
 import styled from "styled-components"
-import Container from "../layout/container/Container"
 
 const StyledSection = styled.main`
   margin-top: 60px;
