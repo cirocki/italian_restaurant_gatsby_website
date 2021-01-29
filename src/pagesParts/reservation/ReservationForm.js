25a1aadc1588021c4261741c248d7fe1b1f75e1b
cirocki <marcincirocki89@gmail.com>
2021-01-29 20:49:35 +0100

    form styling

--- src/pagesParts/reservation/ReservationForm.js
+++ src/pagesParts/reservation/ReservationForm.js
@@ -1,45 +1,45 @@
-import React from "react"
-import { useForm, Controller } from "react-hook-form"
+import React, { useState } from "react"
 import styled from "styled-components"
-
-import TextField from "@material-ui/core/TextField"
-import { makeStyles } from "@material-ui/core/styles"
-import "date-fns"
-
+import { useForm, Controller } from "react-hook-form"
 import DateFnsUtils from "@date-io/date-fns"
+import TextField from "@material-ui/core/TextField"
 import {
   MuiPickersUtilsProvider,
   KeyboardDatePicker,
 } from "@material-ui/pickers"
 
-const useStyles = makeStyles(theme => ({
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: 200,
-  },
-}))
-
 const StyledFormWrapper = styled.div`
   padding: 4rem;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`
+const StyledInputWrapper = styled.div`
+  padding: 10px;
+  width: 100%;
 `
 
 export default function ReservationForm() {
   const { register, handleSubmit, errors, control } = useForm()
-  const onSubmit = data =>
+  const onSubmit = data => {
+    const { name, surname, guests, phone, day, time, more } = data
+    let myDate =
+      day.getUTCDate() + "/" + (day.getMonth() + 1) + "/" + day.getUTCFullYear()
+
     alert(
-      `Thank You ${data.name}!
-      \nYou booked a table for ${data.guests} persons. 
+      `Thank You ${name}!
+      \nYou booked a table for ${guests} persons.
       \nReservation details:
-      \nName: ${data.name},
-      \nSurname: ${data.surname}
-      \nPhone: ${data.phone} 
-      \nGuests: ${data.guests} 
-      \nDate: ${data.date} 
-      \nTime: ${data.time} 
-      \nAdditional Info: ${data.more}   
+      \nName: ${name},
+      \nSurname: ${surname}
+      \nPhone: ${phone}
+      \nGuests: ${guests}
+      \nDate: ${myDate}
+      \nTime: ${time}
+      \nAdditional Info: ${more}
       `
     )
+  }
 
   let switchErrorMsg = errors => {
     switch (errors) {
@@ -56,17 +56,10 @@ export default function ReservationForm() {
     }
   }
 
-  const [selectedDate, setSelectedDate] = React.useState(new Date())
-
-  const handleDateChange = date => {
-    setSelectedDate(date)
-  }
-
-  const classes = useStyles()
   return (
     <StyledFormWrapper>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
+        <StyledInputWrapper>
           <TextField
             inputRef={register({ required: true, minLength: 3 })}
             type="text"
@@ -75,10 +68,11 @@ export default function ReservationForm() {
             label="Name"
             variant="outlined"
             color="primary"
+            fullWidth={true}
             helperText={switchErrorMsg(errors.name?.type)}
           />
-        </div>
-        <div>
+        </StyledInputWrapper>
+        <StyledInputWrapper>
           <TextField
             inputRef={register({ required: true, minLength: 3 })}
             type="text"
@@ -87,66 +81,50 @@ export default function ReservationForm() {
             label="Surname"
             variant="outlined"
             color="primary"
+            fullWidth={true}
             helperText={switchErrorMsg(errors.surname?.type)}
           />
-        </div>
-        <div>
+        </StyledInputWrapper>
+        <StyledInputWrapper>
           <TextField
             inputRef={register({ required: true, minLength: 3 })}
             type="text"
             name="phone"
             id="phone"
-            label="Phone number"
+            label="Phone"
             variant="outlined"
             color="primary"
+            fullWidth={true}
             helperText={switchErrorMsg(errors.phone?.type)}
           />
-        </div>
+        </StyledInputWrapper>
 
-        <TextField
-          inputRef={register({ required: true, min: 2, max: 20 })}
-          helperText={switchErrorMsg(errors.guests?.type)}
-          name="guests"
-          id="guests"
-          label="Guests"
-          type="number"
-          variant="outlined"
-        />
-        <TextField
-          inputRef={register}
-          name="more"
-          id="more"
-          label="Additional Info"
-          multiline
-          rows={8}
-          variant="outlined"
-        />
-
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <Controller
-            name="date"
-            control={control}
-            defaultValue={new Date()}
-            render={() => (
-              <KeyboardDatePicker
-                disableToolbar
-                format="dd/MM/yyyy"
-                margin="normal"
-                inputVariant="outlined"
-                name="date"
-                id="date"
-                label="Date"
-                value={selectedDate}
-                onChange={handleDateChange}
-                KeyboardButtonProps={{
-                  "aria-label": "change date",
-                }}
-              />
-            )}
-          />
-        </MuiPickersUtilsProvider>
-
-        <div>
+        <StyledInputWrapper>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Controller
+              name="day"
+              control={control}
+              defaultValue={new Date()}
+              render={({ onChange, value }) => (
+                <KeyboardDatePicker
+                  disableToolbar
+                  format="dd/MM/yyyy"
+                  margin="normal"
+                  inputVariant="outlined"
+                  name="day"
+                  id="day"
+                  label="Date"
+                  value={value}
+                  onChange={onChange}
+                  KeyboardButtonProps={{
+                    "aria-label": "change date",
+                  }}
+                />
+              )}
+            />
+          </MuiPickersUtilsProvider>
+        </StyledInputWrapper>
+        <StyledInputWrapper>
           <TextField
             inputRef={register}
             name="time"
@@ -155,7 +133,8 @@ export default function ReservationForm() {
             type="time"
             variant="outlined"
             defaultValue="18:00"
-            className={classes.textField}
+            fullWidth={true}
+            // className={classes.textField}
             InputLabelProps={{
               shrink: true,
             }}
@@ -163,11 +142,35 @@ export default function ReservationForm() {
               step: 600, // 10 min
             }}
           />
-        </div>
+        </StyledInputWrapper>
 
-        <div>
-          <button type="submit">Make reservation</button>
-        </div>
+        <StyledInputWrapper>
+          <TextField
+            inputRef={register({ required: true, min: 2, max: 20 })}
+            helperText={switchErrorMsg(errors.guests?.type)}
+            name="guests"
+            id="guests"
+            label="Guests"
+            type="number"
+            variant="outlined"
+            fullWidth={true}
+          />
+        </StyledInputWrapper>
+
+        <StyledInputWrapper>
+          <TextField
+            inputRef={register}
+            name="more"
+            id="more"
+            label="Additional Info"
+            multiline
+            rows={7}
+            variant="outlined"
+            fullWidth={true}
+          />
+        </StyledInputWrapper>
+
+        <button type="submit">Make reservation</button>
       </form>
     </StyledFormWrapper>
   )
