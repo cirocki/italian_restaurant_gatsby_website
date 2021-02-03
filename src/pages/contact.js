cab06386d1da223fdcf855571ca546a9619bb55a
cirocki <marcincirocki89@gmail.com>
2021-02-03 18:55:26 +0100

    contact page styling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,16 +2,11 @@ import React from "react"
 import Layout from "../layout/Layout"
 import { Helmet } from "react-helmet"
 import styled from "styled-components"
-import Container from "../layout/container/Container"
 import ContactHeader from "../pagesParts/contact/ContactHeader"
 import MyMap from "../pagesParts/contact/MyMap"
 
 const StyledMainDiv = styled.main`
-  padding: 4rem 0;
-`
-const StyledMapWrapper = styled.main`
-  height: 480px;
-  width: auto;
+  margin-top: 60px;
 `
 
 export default function ContactPage() {
@@ -22,11 +17,7 @@ export default function ContactPage() {
       </Helmet>
       <StyledMainDiv>
         <ContactHeader />
-        <Container>
-          <StyledMapWrapper>
-            <MyMap />
-          </StyledMapWrapper>
-        </Container>
+        <MyMap />
       </StyledMainDiv>
     </Layout>
   )
--- src/pagesParts/contact/MyMap.js
+++ src/pagesParts/contact/MyMap.js
@@ -1,15 +1,63 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
+import styled from "styled-components"
+import Container from "../../layout/container/Container"
+
+const StyledMainWrapper = styled.div`
+  padding: 120px 0;
+  position: relative;
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: calc(50% - 360px);
+    height: 100%;
+    background: ${props => props.theme.colors.light};
+    z-index: -1;
+    @media (max-width: 1069px) {
+      display: none;
+    }
+  }
+`
+
+const StyledMainGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: repeat(6, 1fr);
+`
+const StyledImgDiv = styled.div`
+  grid-column: 9/13;
+  grid-row: 1/7;
+`
+const StyledFindDiv = styled.div`
+  grid-column: 1/4;
+  grid-row: 1/2;
+`
+const StyledHoursDiv = styled.div`
+  grid-column: 5/8;
+  grid-row: 1/2;
+`
+const StyledInfoDiv = styled.div`
+  grid-column: 1/9;
+  grid-row: 3/6;
+  background: ${props => props.theme.colors.dark};
+`
+const StyledGoldDiv = styled.div`
+  grid-column: 2/9;
+  grid-row: 6/7;
+  background: ${props => props.theme.colors.gold};
+`
 
 export default function MyMap() {
   const data = useStaticQuery(graphql`
     query {
       image2: file(
-        relativePath: { eq: "subpages/contact/mondello-map-screen.jpg" }
+        relativePath: { eq: "subpages/contact/mondello-contact-image.jpg" }
       ) {
         childImageSharp {
-          fluid(maxHeight: 320, quality: 100) {
+          fluid(maxHeight: 720, quality: 100) {
             ...GatsbyImageSharpFluid_withWebp
             ...GatsbyImageSharpFluidLimitPresentationSize
           }
@@ -18,12 +66,37 @@ export default function MyMap() {
     }
   `)
   return (
-    <div>
-      <h1>mapa</h1>
-      <Img
-        fluid={data.image2.childImageSharp.fluid}
-        alt="Italian Traditional mask"
-      />
-    </div>
+    <StyledMainWrapper>
+      <Container>
+        <StyledMainGrid>
+          <StyledInfoDiv>
+            <h1>mondello</h1>
+          </StyledInfoDiv>
+          <StyledGoldDiv></StyledGoldDiv>
+          <StyledFindDiv>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+              blanditiis, tenetur animi deleniti dolorem quas omnis eius
+              delectus quibusdam culpa alias accusantium quisquam repudiandae
+              velit! Ipsam sequi ullam optio impedit?
+            </p>
+          </StyledFindDiv>
+          <StyledHoursDiv>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+              blanditiis, tenetur animi deleniti dolorem quas omnis eius
+              delectus quibusdam culpa alias accusantium quisquam repudiandae
+              velit! Ipsam sequi ullam optio impedit?
+            </p>
+          </StyledHoursDiv>
+          <StyledImgDiv>
+            <Img
+              fluid={data.image2.childImageSharp.fluid}
+              alt="Italian Traditional mask"
+            />
+          </StyledImgDiv>
+        </StyledMainGrid>
+      </Container>
+    </StyledMainWrapper>
   )
 }
